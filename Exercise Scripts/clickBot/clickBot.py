e3a840037a3bad4750336c7628ab4ad92c66064e
Semih Eryücel <76877013+semihi@users.noreply.github.com>
2024-07-02 15:41:36 +0300

    Update clickBot.py
    Changed detection system.
    Detection is now based on color filtered screenshots instead of image recognition.

--- Exercise Scripts/clickBot/clickBot.py
+++ Exercise Scripts/clickBot/clickBot.py
@@ -1,11 +1,11 @@
 from pyautogui import *
 import pyautogui
+import cv2
+import numpy as np
 import time
 import keyboard
-import random
 import win32api, win32con
 import threading
-
 
 def click(x, y):
     win32api.SetCursorPos((x, y))
@@ -15,31 +15,59 @@ def click(x, y):
 
 print("started")
 
-targetImg = r"C:\Users\semih\Projects\python-exercises\Exercise Scripts\clickBot\target.png"
+targetImg = cv2.imread(r"C:\Users\semih\Projects\python-exercises\Exercise Scripts\clickBot\target.png")
 
-stopEvent = threading.Event()
+stop_event = threading.Event()
 
 def checkStop():
-    while not stopEvent.is_set():
+    while not stop_event.is_set():
         if keyboard.is_pressed('q'):
-            stopEvent.set()
+            stop_event.set()
         time.sleep(0.05)
 
 def scan(region):
-    while not stopEvent.is_set():
-        try:
-            target = pyautogui.locateCenterOnScreen(targetImg, confidence=0.85, region=region)
-            if target:
-                click(target[0], target[1] + 22)
-            time.sleep(0.08)
-        except pyautogui.ImageNotFoundException:
-            continue
+    time.sleep(region[1]/1000)
+    while not stop_event.is_set():
+        # contours = []  #Buna muhtemelen gerek yok
+        scrSht = np.array(pyautogui.screenshot(region=region))
+        scrHsv = cv2.cvtColor(scrSht, cv2.COLOR_RGB2HSV)
+        maskGreen = cv2.inRange(scrHsv, (29, 85, 215), (47,255,255))
+        # imaskGreen = maskGreen > 0
+        # green = np.zeros_like(screen, np.uint8)
+        # green[imaskGreen] = screen[imaskGreen]
 
-regions = [(5, 140, 420, 165), (5, 305, 420, 165), (5, 470, 420, 165), (5, 635, 420, 165), (5, 800, 420, 165)]
+        cv2.imshow("maskGreen "+str(region), maskGreen)
+        cv2.waitKey(1)
+        
+        contours, _ = cv2.findContours(maskGreen, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
 
-threads = [threading.Thread(target=checkStop)]
-for region in regions:
-    threads.append(threading.Thread(target=scan, args=(region,)))
+        # Iterate through each contour and find its center
+        for contour in contours:
+
+            if cv2.contourArea(contour) >= 60:
+                # Compute the moments of the contour
+                M = cv2.moments(contour)
+                
+                # Calculate the center of the contour
+                if M["m00"] != 0:
+                    cX = int(M["m10"] / M["m00"])
+                    cY = int(M["m01"] / M["m00"])
+                    
+                    # Click on the center of the contour *using PyAutoGUI*
+                    click(5+cX, 140+cY+18)
+                    time.sleep(0.001)
+
+                    # For visualization purposes (optional)
+                    # cv2.circle(maskGreen, (cX, cY), 5, (255, 0, 0), -1)
+        time.sleep(0.01)
+        # Display the result (optional)
+        # cv2.imshow('Centers ', maskGreen)
+        # cv2.waitKey(1)
+
+# regions = [(5, 140, 440, 205), (5, 345, 440, 205), (5, 550, 440, 205), (5, 755, 440, 205)]
+region = (5,140,440,820)
+
+threads = [threading.Thread(target=checkStop), threading.Thread(target=scan, args=(region,))]
 
 for thread in threads:
     thread.start()
@@ -48,82 +76,3 @@ for thread in threads:
     thread.join()
 
 print("quit")
-
-
-
-
-
-
-
-
-
-
-
-
-# def click(x,y):
-#     win32api.SetCursorPos((x,y))
-#     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
-#     time.sleep(0.003)
-#     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
-
-
-# print("started")
-
-# targetImg = r"C:\Users\semih\Projects\python-exercises\Exercise Scripts\clickBot\target.png"
-
-        
-
-# def scan1():
-#     while not keyboard.is_pressed('q'):
-#         try:
-#             target = pyautogui.locateCenterOnScreen(targetImg, confidence=0.83, region=(5,140,420,205))
-#             click(target[0],target[1]+20)
-#             time.sleep(0.08)
-#         except pyautogui.ImageNotFoundException:
-#             continue
-
-# def scan2():
-#     while not keyboard.is_pressed('q'):
-#         try:
-#             target = pyautogui.locateCenterOnScreen(targetImg, confidence=0.83, region=(5,345,420,205))
-#             click(target[0],target[1]+20)
-#             time.sleep(0.08)
-#         except pyautogui.ImageNotFoundException:
-#             continue
-
-# def scan3():
-#     while not keyboard.is_pressed('q'):
-#         try:
-#             target = pyautogui.locateCenterOnScreen(targetImg, confidence=0.83, region=(5,550,420,205))
-#             click(target[0],target[1]+20)
-#             time.sleep(0.08)
-#         except pyautogui.ImageNotFoundException:
-#             continue
-
-# def scan4():
-#     while not keyboard.is_pressed('q'):
-#         try:
-#             target = pyautogui.locateCenterOnScreen(targetImg, confidence=0.83, region=(5,755,420,205))
-#             click(target[0],target[1]+20)
-#             time.sleep(0.08)
-#         except pyautogui.ImageNotFoundException:
-#             continue
-
-
-# thread_1 = threading.Thread(target=scan1)
-# thread_2 = threading.Thread(target=scan2)
-# thread_3 = threading.Thread(target=scan3)
-# thread_4 = threading.Thread(target=scan4)
-
-# thread_1.start()
-# thread_2.start()
-# thread_3.start()
-# thread_4.start()
-
-# thread_1.join()
-# thread_2.join()
-# thread_3.join()
-# thread_4.join()
-
-
-# print("quit")

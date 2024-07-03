95c2d4607210f6eaeb7047f27d90c8a27028dc87
Semih Eryücel <76877013+semihi@users.noreply.github.com>
2024-07-03 20:27:28 +0300

    Update clickBot.py
    Added features below:
    - Bomb detection
    - Pause when game ends
    - Click accuracy improved

--- Exercise Scripts/clickBot/clickBot.py
+++ Exercise Scripts/clickBot/clickBot.py
@@ -6,18 +6,29 @@ import time
 import keyboard
 import win32api, win32con
 import threading
+import math
 
-def click(x, y):
-    win32api.SetCursorPos((x, y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
-    time.sleep(0.005)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+def click(x, y, bombs):
+    abort = False
+    for bomb in bombs:
+        if math.dist([x,y],bomb) < 90:
+            # print("abort")
+            abort = True
+            break
+    if not abort and not pause_event.is_set():
+        win32api.SetCursorPos((x, y))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+        time.sleep(0.006)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+        time.sleep(0.001)
+    # elif pause_event.is_set():
+    #     print("on pause")
+
 
 print("started")
 
-targetImg = cv2.imread(r"C:\Users\semih\Projects\python-exercises\Exercise Scripts\clickBot\target.png")
-
 stop_event = threading.Event()
+pause_event = threading.Event()
 
 def checkStop():
     while not stop_event.is_set():
@@ -25,49 +36,65 @@ def checkStop():
             stop_event.set()
         time.sleep(0.05)
 
-def scan(region):
-    time.sleep(region[1]/1000)
+def checkGameOver():
     while not stop_event.is_set():
-        # contours = []  #Buna muhtemelen gerek yok
-        scrSht = np.array(pyautogui.screenshot(region=region))
-        scrHsv = cv2.cvtColor(scrSht, cv2.COLOR_RGB2HSV)
-        maskGreen = cv2.inRange(scrHsv, (29, 85, 215), (47,255,255))
-        # imaskGreen = maskGreen > 0
-        # green = np.zeros_like(screen, np.uint8)
-        # green[imaskGreen] = screen[imaskGreen]
+        if pyautogui.pixel(375,741) > (243,243,243) or pyautogui.pixel(355,930) > (243,243,243):
+            pause_event.set()
+            # print("paused")
+        else:
+            pause_event.clear()
+            # print("resumed")
+        time.sleep(0.005)
 
-        cv2.imshow("maskGreen "+str(region), maskGreen)
-        cv2.waitKey(1)
+
+def scan(region):
+    while not stop_event.is_set():
+        scrRgb = np.array(pyautogui.screenshot(region=region))
+        scrHsv = cv2.cvtColor(scrRgb, cv2.COLOR_RGB2HSV)
+        # scrBgr = cv2.cvtColor(scrRgb, cv2.COLOR_RGB2BGR)
+        maskGreen = cv2.inRange(scrHsv, (29, 85, 215), (47,255,255))
+        maskBomb = cv2.inRange(scrRgb, (127,122,119), (180,176,177))
+        # cv2.imshow("maskGreen ", maskGreen)
+        # cv2.waitKey(1)
+        # cv2.imshow("maskBomb ", maskBomb)
+        # cv2.waitKey(1)
         
         contours, _ = cv2.findContours(maskGreen, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+        contoursBomb, _ = cv2.findContours(maskBomb, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
 
-        # Iterate through each contour and find its center
-        for contour in contours:
-
-            if cv2.contourArea(contour) >= 60:
-                # Compute the moments of the contour
-                M = cv2.moments(contour)
-                
-                # Calculate the center of the contour
+        bombs = []
+        for bomb in contoursBomb:
+            if cv2.contourArea(bomb) >= 40:
+                M = cv2.moments(bomb)
                 if M["m00"] != 0:
                     cX = int(M["m10"] / M["m00"])
                     cY = int(M["m01"] / M["m00"])
-                    
-                    # Click on the center of the contour *using PyAutoGUI*
-                    click(5+cX, 140+cY+18)
-                    time.sleep(0.001)
+                    bombs.append([5+cX,140+cY+20])
+                    # cv2.circle(scrBgr, (cX, cY), 4, (255, 10, 30), -1)
 
-                    # For visualization purposes (optional)
-                    # cv2.circle(maskGreen, (cX, cY), 5, (255, 0, 0), -1)
-        time.sleep(0.01)
-        # Display the result (optional)
-        # cv2.imshow('Centers ', maskGreen)
+        i = 0
+        for contour in contours:
+            if cv2.contourArea(contour) >= 60:
+                M = cv2.moments(contour)
+                
+                if M["m00"] != 0:
+                    cX = int(M["m10"] / M["m00"])
+                    cY = int(M["m01"] / M["m00"])
+
+                    click(5+cX, 140+cY+20, bombs)
+                    time.sleep(0.002)
+                    # cv2.circle(scrBgr, (cX, cY), 4, (10, 10, 255), -1)
+                    
+                    i += 1
+                    if i == 4:
+                        break                
+        time.sleep(0.005)
+        # cv2.imshow('Centers ', scrBgr)
         # cv2.waitKey(1)
 
-# regions = [(5, 140, 440, 205), (5, 345, 440, 205), (5, 550, 440, 205), (5, 755, 440, 205)]
 region = (5,140,440,820)
 
-threads = [threading.Thread(target=checkStop), threading.Thread(target=scan, args=(region,))]
+threads = [threading.Thread(target=checkStop), threading.Thread(target=scan, args=(region,)), threading.Thread(target=checkGameOver)]
 
 for thread in threads:
     thread.start()

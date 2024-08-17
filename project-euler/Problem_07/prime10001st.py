581ebabe9e6c76a2573af379a7b5dd881529acaa
Semih Eryücel <76877013+semihi@users.noreply.github.com>
2024-08-17 19:14:59 +0300

    Update prime10001st.py

--- project-euler/Problem_07/prime10001st.py
+++ project-euler/Problem_07/prime10001st.py
@@ -1,18 +1,16 @@
-    primes = [2]
-    primeCount = 1
-    isDivisible = False
-    i = 3
-
-    while primeCount < 10001:
-        for prime in primes:
-            if i%prime == 0:
-                isDivisible = True
-                break
-        if not isDivisible:
-            primes.append(i)
-            primeCount += 1
-        else:
-            isDivisible = False
-        i += 2
-
-    print(primes[-1])
+primes = [2]
+primeCount = 1
+isDivisible = False
+i = 3
+while primeCount < 10001:
+    for prime in primes:
+        if i%prime == 0:
+            isDivisible = True
+            break
+    if not isDivisible:
+        primes.append(i)
+        primeCount += 1
+    else:
+        isDivisible = False
+    i += 2
+print(primes[-1])

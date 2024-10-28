f33eb62a18339d3cd3be0f11092d3f8777295397
Semih Eryücel <76877013+semihery@users.noreply.github.com>
2024-10-28 23:18:18 +0300

    Reapply "Readjusted clickBot.py"
    This reverts commit c89cfd54ca0bd0db7094b92e923436e7af722f52.

--- neural-network-exercises/get-price-history.py
+++ neural-network-exercises/get-price-history.py
@@ -1,4 +1,11 @@
 import requests
+import time
+
+dataStartTime = 1704110400000
+dataLastTime = 1704110400000
+currentTime = 1729087900000
+# currentTime = int(round(time.time() * 1000))
+# print(currentTime)
 
 base_url = "https://api.binance.com"
 endpoint = "/api/v3/klines"
@@ -6,16 +13,19 @@ params = {
     "symbol": "TONUSDT",
     "interval": "1m",  # Interval (e.g., 1m, 5m, 1h, 1d)
     "limit": 1000,  # Number of data points to retrieve
-    #"startTime": 1725391587067,  # Start time in UNIX timestamp (optional)
-    #"endTime": 1725392587067  # End time in UNIX timestamp (optional)
+    "startTime": 0,  # Start time in UNIX timestamp (optional)
+    # "endTime": 0  # End time in UNIX timestamp (optional)
 }
 
-response = requests.get(base_url + endpoint, params=params)
-data = response.json()
-# print(data)
-
-avgPriceHistory = []
-for i in data:
-    avgPriceHistory.append((float(i[2])+float(i[3]))/2)
-
-print(avgPriceHistory)
+while dataLastTime < currentTime:
+    avgPriceHistory = []
+    params["startTime"] = dataLastTime
+    response = requests.get(base_url + endpoint, params=params)
+    data = response.json()
+    for i in data:
+        avgPriceHistory.append((float(i[2])+float(i[3]))/2)
+        
+    dataLastTime = data[-1][6]
+    with open("ton_1704110400000_1729087900000.txt", "a") as file:
+        for value in avgPriceHistory:
+            file.write(f"{value}\n")
